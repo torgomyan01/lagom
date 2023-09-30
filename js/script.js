6c94272b590046adda3542267f211b6f144682ce
armanpinachyan <pinachyan99@mail.ru>
2023-09-30 19:08:36 +0400

    19:08

--- js/script.js
+++ js/script.js
@@ -23,3 +23,98 @@ $(window).scroll();
 
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+
+
+
+/// Gallery
+gsap.registerPlugin(ScrollToPlugin, ScrollTrigger);
+
+/* Main navigation */
+let panelsSection = document.querySelector("#panels"),
+  panelsContainer = document.querySelector("#panels-container"),
+  tween;
+
+// document.querySelectorAll(".anchor").forEach(anchor => {
+//     anchor.addEventListener("click", function(e) {
+//         e.preventDefault();
+//         let targetElem = document.querySelector(e.target.getAttribute("href")),
+//           y = targetElem;
+//         if (targetElem && panelsContainer.isSameNode(targetElem.parentElement)) {
+//             let totalScroll = tween.scrollTrigger.end - tween.scrollTrigger.start,
+//               totalMovement = (panels.length - 1) * targetElem.offsetWidth;
+//             y = Math.round(tween.scrollTrigger.start + (targetElem.offsetLeft / totalMovement) * totalScroll);
+//         }
+//         gsap.to(window, {
+//             scrollTo: {
+//                 y: y,
+//                 autoKill: false
+//             },
+//             duration: 1
+//         });
+//     });
+// });
+
+
+/* Panels */
+const panels = gsap.utils.toArray("#panels-container .panel");
+tween = gsap.to(panels, {
+    xPercent: -100 * ( panels.length - 1 ),
+    ease: "none",
+    scrollTrigger: {
+        trigger: "#panels-container",
+        pin: true,
+        start: "top top",
+        scrub: 1,
+        snap: {
+            snapTo: 1 / (panels.length - 1),
+            inertia: false,
+            duration: {min: 0.1, max: 0.1}
+        },
+        end: () =>  "+=" + (panelsContainer.offsetWidth - innerWidth)
+    }
+});
+
+
+gsap.to(".img-1", {
+    y: 100,
+    duration: 2,
+    ease: "elastic",
+    scrollTrigger: {
+        trigger: ".img-1",
+        containerAnimation: tween,
+        start: "left center",
+        toggleActions: "play none none reset"
+    }
+});
+
+
+// let elements = gsap.utils.toArray(document.querySelectorAll(".panel h2"));
+// let scrollTriggerTimeline = gsap.timeline();
+//
+// console.log(elements)
+//
+// panels.forEach((panel, i) => {
+//     ScrollTrigger.create({
+//         trigger: panel,
+//         start: "top 50%",
+//
+//         onEnter: () => {
+//             // gsap
+//             //   .timeline()
+//             //   .set(button, { className: "button" }, "+=1")
+//             //   .set(button, { className: "button non-active" }, "+=1");
+//             console.log(panels[i])
+//         },
+//         onEnterBack: () => {
+//             // gsap
+//             //   .timeline()
+//             //   .set(button, { className: "button" }, "+=1")
+//             //   .set(button, { className: "button non-active" }, "+=1");
+//             // gsap.set(".nav__info button", { color: "#0a71e4", fontWeight: "normal" });
+//             // gsap.set(buttons[i], { color: "#FF4087", fontWeight: "bold" });
+//             console.log(panels[i])
+//         }
+//     });
+// });
+
+
