0f18ae7cba78e1538b70275751598c5dead8681d
armanpinachyan <pinachyan99@mail.ru>
2023-10-25 13:09:53 +0400

    13:09

--- js/script.js
+++ js/script.js
@@ -32,6 +32,14 @@ $(window).on('load', function (){
     })
 
 
+    const Planning = $('#planning .heading').offset().top;
+    console.log(Planning)
+    ScrollTrigger.create({
+        start: `${Planning - 50}`,
+        end: `${Planning + 200}`,
+        toggleClass: {className: 'heading-open', targets: '.planning'},
+    });
+
 
 
     const YoulGet = $('#youll-get').offset().top;
@@ -153,62 +161,6 @@ tween = gsap.to(panels, {
     }
 });
 
-
-// gsap.to(".gallery2", {
-//     y: -100,
-//     x: -100,
-//     duration: 2,
-//     ease: "elastic",
-//     scrollTrigger: {
-//         trigger: ".gallery2",
-//         containerAnimation: tween,
-//         start: "left center",
-//         toggleActions: "play none none reset"
-//     }
-// });
-
-// gsap.to(".image-3-1", {
-//     y: -100,
-//     x: -100,
-//     duration: 2,
-//     ease: "elastic",
-//     scrollTrigger: {
-//         trigger: ".image-3-1",
-//         containerAnimation: tween,
-//         start: "left center",
-//         toggleActions: "play none none reset"
-//     }
-// });
-
-
-// let elements = gsap.utils.toArray(document.querySelectorAll(".panel h2"));
-// let scrollTriggerTimeline = gsap.timeline();
-//
-// console.log(elements)
-//
-// panels.forEach((panel, i) => {
-//     ScrollTrigger.create({
-//         trigger: panel,
-//         start: "top 50%",
-//
-//         onEnter: () => {
-//             // gsap
-//             //   .timeline()
-//             //   .set(button, { className: "button" }, "+=1")
-//             //   .set(button, { className: "button non-active" }, "+=1");
-//             console.log(panels[i])
-//         },
-//         onEnterBack: () => {
-//             // gsap
-//             //   .timeline()
-//             //   .set(button, { className: "button" }, "+=1")
-//             //   .set(button, { className: "button non-active" }, "+=1");
-//             // gsap.set(".nav__info button", { color: "#0a71e4", fontWeight: "normal" });
-//             // gsap.set(buttons[i], { color: "#FF4087", fontWeight: "bold" });
-//             console.log(panels[i])
-//         }
-//     });
-// });
 
 
 
@@ -337,83 +289,12 @@ function ChangeSlider(numStatus){
 
 
 
-// const scrollTop = $('#youll-get').offset().top;
-// const contHeight = $('#container').height();
-// const panelss = gsap.utils.toArray(".panel-youl");
-//
-//
-// gsap.to(".panel-youl", {
-//     yPercent: -100,
-//     ease: "slow",
-//     stagger: 1,
-//     scrollTrigger: {
-//         trigger: "#container",
-//         start: "top +90",
-//         end: `${scrollTop + (panelss.length * contHeight)}`,
-//         scrub: 0,
-//         pin: true,
-//     }
-// });
-//
-//
-// gsap.set(".panel-youl", { zIndex: (i, target, targets) => targets.length - i });
-// const navLinks = gsap.utils.toArray(".section_nav a");
-// navLinks.forEach((link, i) => {
-//     link.addEventListener("mouseover", e => {
-//         e.preventDefault();
-//         console.log(i)
-//         gsap.to(window,
-//           {
-//               scrollTo: scrollTop + (i * contHeight)
-//           });
-//     });
-// });
-//
-// panelss.forEach((panel, i) => {
-//     ScrollTrigger.create({
-//         start: 0,
-//         end: `${scrollTop + (i * contHeight) + contHeight}`,
-//         onLeave: () => {
-//             if(navLinks[i + 1]) {
-//                 gsap.to(navLinks[i + 1], {scale: 1.3, color: "#303030"});
-//                 gsap.to(navLinks[i], {scale: 1, color: "#30303099"});
-//             }
-//         },
-//         onEnterBack: () => {
-//             gsap.to(navLinks[i], {scale: 1.3, color: "#303030"});
-//             if(navLinks[i + 1]) {
-//                 gsap.to(navLinks[i + 1], {scale: 1, color: "#30303099"});
-//             }
-//         },
-//     })
-// });
-//
-//
 
 
 const panelYoul = $('.panel-youl');
 const panelNav = $('.section_nav a');
 const youlTexts = $('.box-title .title1');
 
-// panelNav.on('mouseenter', function (e){
-//     e.preventDefault();
-//     const thisElement = $(this);
-//     const href = thisElement.attr('href');
-//     panelYoul.addClass('d-none').removeClass('active');
-//     $(href).removeClass('d-none');
-//
-//     youlTexts.addClass('d-none').removeClass('active');
-//     $(`#text-${href.replace(/#/g, '')}`).removeClass('d-none')
-//
-//
-//     setTimeout(() => {
-//         $(href).addClass('active')
-//         $(`#text-${href.replace(/#/g, '')}`).addClass('active')
-//     }, 100);
-//
-//     panelNav.removeClass('active');
-//     thisElement.addClass('active');
-// })
 
 
 panelNav.on('click', function (e){
@@ -425,8 +306,8 @@ panelNav.on('click', function (e){
 // ........ slick
 
 const allslicLengt = document.querySelectorAll('.slider .image')
-const span_count = document.querySelector('.count-box samp')
-span_count?.innerText = `/ ${allslicLengt.length} `
+const span_count = document.querySelector('.count samp')
+span_count.innerText = `/ ${allslicLengt.length} `
 let activeIndex = 1;
 
 $('.slider').slick({
@@ -468,9 +349,7 @@ $('.slider').slick({
                 slidesToScroll: 1,
             }
         }
-        // You can unslick at a given breakpoint now by adding:
-        // settings: "unslick"
-        // instead of a settings object
+
     ]
 }).on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
     //currentSlide is undefined on init -- set it to 0 in this case (currentSlide is 0 based)
